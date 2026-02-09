15b7a3f28c9b9af95bcbf9f28bff8907dfd9f767
Jakub Chrobok <72559236+jakub4335@users.noreply.github.com>
2026-02-09 01:20:53 +0100

    Time: 0 ms (100.00%) | Memory: 20.1 MB (7.85%) - LeetSync

--- 66-plus-one/plus-one.swift
+++ 66-plus-one/plus-one.swift
@@ -1,24 +1,19 @@
 class Solution {
     func plusOne(_ digits: [Int]) -> [Int] {
-        // var digitsArray = [Int]()
-        // digitsArray = digits.map { $0 }
-        var digitsArray = digits
-        let digtisLength = digitsArray.count - 1 
-        for (index, i) in digitsArray.reversed().enumerated() {
-            if ( i != 9) { 
-                digitsArray[digtisLength - index] = i + 1
-                return digitsArray
+        var digits = digits
+        var digitsCount = digits.count - 1 // jezeli jest 5 cyfr, no licząc od 0 jest to cztery
+        var i = 0
+        while 0 <=  digitsCount {
+
+            if (digits[digitsCount] != 9) { // zwiekszam o 1
+                digits[digitsCount] += 1
+                return digits
             }
 
-            if ( i == 9) {
-                digitsArray[digtisLength - index] = 0
-            }
-            
-            if ( i == 9 && (digtisLength - index) == 0) {
-                digitsArray.insert((1), at: 0)
-                return digitsArray
-            }
+            digits[digitsCount] = 0
+            digitsCount -= 1
         }
-       return digitsArray
+
+        return [1] + digits
     }
 }
