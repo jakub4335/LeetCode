c6a14862173e17527a8a38c78030e949eb9a51b8
Jakub Chrobok <72559236+jakub4335@users.noreply.github.com>
2026-02-16 01:12:45 +0100

    Time: 4 ms (13.66%) | Memory: 20.2 MB (29.19%) - LeetSync

--- 67-add-binary/add-binary.swift
+++ 67-add-binary/add-binary.swift
@@ -3,9 +3,8 @@ class Solution {
 
         let aLength: Int = a.length
         let bLength: Int = b.length
-        let finalArray = [String]()
         var finalString: String = ""
-        var i: Int = 0
+        //var i: Int = 0
         let maxLength: Int = max(aLength,bLength) // ilosc calkowitych petli
 
         let paddedA = String(repeating: "0", count: maxLength - aLength) + a
@@ -13,16 +12,8 @@ class Solution {
 
         let arrayA = paddedA.map { String($0) }
         let arrayB = paddedB.map { String($0) }
-
-        print(arrayA)
-        print(arrayB)
-
-
-
-
+        
         var storage: [[String]] = Array(repeating: [], count: maxLength)
-        //print(biggerLength)
-
 
         for i in (0...maxLength-1).reversed(){ 
             if (arrayA[i] == "1") {
@@ -32,16 +23,15 @@ class Solution {
                 storage[i].append(arrayB[i])
             }
 
-            if (storage[i].count == 2) { // są dwie jedynki
-                if (i == 0 ){
+            if (storage[i].count == 2) {
+                if (i == 0 ) {
                     finalString.append("01")
-                    print("teraz to wynik \(finalString)")
                     break
                 }
                 finalString.append("0")
                 storage[i-1].append("1")
             }
-            else if (storage[i].count == 3) { // są dwie jedynki
+            else if (storage[i].count == 3) {
                 if (i == 0 ){
                     finalString.append("11")
                     print("teraz to wynik \(finalString)")
@@ -53,20 +43,10 @@ class Solution {
             else if (storage[i].count == 1) {
                 finalString.append("1")
             }
-            else{
+            else {
                 finalString.append("0")
-            }
-
-            
-            
+            }       
         }
-
-        print("bez zmiany to  \(finalString)")
-       // return finalString.reversed()
-        return (String(finalString.reversed()))
-
-        
-        
-        
+        return (String(finalString.reversed())) 
     }
 }
