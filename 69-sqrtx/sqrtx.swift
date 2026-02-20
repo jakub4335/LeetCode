bffeb4732f09af339e75850193b807eda1b8c914
Jakub Chrobok <72559236+jakub4335@users.noreply.github.com>
2026-02-20 20:49:42 +0100

    Time: 22 ms (7.14%) | Memory: 19.1 MB (87.95%) - LeetSync

--- 69-sqrtx/sqrtx.swift
+++ 69-sqrtx/sqrtx.swift
@@ -1,60 +1,16 @@
 class Solution {
     func mySqrt(_ x: Int) -> Int {
-        let newX: Int = x
         var result : Int
-
-        for i in 0...newX {
-            if (i * i) == newX {
+        for i in 0...x {
+            if (i * i) == x {
                 result = i
                 return result
             }
-
-            if (i * i) > newX {
+            if (i * i) > x {
                 result = i - 1
                 return result
-                
             }
-
         }
-        
-
         return 0
-
-
-        // while newX > currentDivider {
-            
-        //     if currentDivider = smallestDivisision(newX) {
-        //         niceArray.append[currentDivider] // dodaje pierwszy i kolejny dzielnik do tablicy
-        //         print("obecna liczba, na której operuje to \(newX), jej dzielnik to \(currentDivider)")
-        //         newX = newx/currentDivider // zmniejszam obecną liczbe dzieąc ją, np 300 
-
-        //     } // zwraca pierwszy dzielnik
-        //     else {
-        //         niceArray.append[newX]
-        //         break // to sie dzieje, jak liczba jest liczbą pierwszą, lub po prostu nie ma już wiecej do 
-        //     }
-        // }
-
-
-
-
-
-        // var newX1: (Int) = smallestDivisision(newX)
-        // return newX1
-
-
-
-
     }
-
-
-    // func smallestDivisision(_ newX: Int) -> Int {
-    //     if let smallestDivision = (2...newX).first(where: {newX % $0 == 0}) { // zwraca pierwszą podzielną liczbę, 
-    //         return smallestDivision
-    //     }
-    //     else { // jeżeli nie ma żadnej podzielnej liczby , nawet dwójki, to znaczy, że jest to liczba pierwsza i jej pierwiastkiem 
-    //             // kwadratowym jest jej własny pierwiastek, np pierwiastek z 7
-    //         return newX
-    //     }
-    // }
 }
